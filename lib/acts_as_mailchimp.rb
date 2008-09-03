cad750db77fbc83b1a0ef1ede42a4da18c5a2466
Brian Getting <brian@terra-firma-design.com>
2008-09-03 11:13:37 -0700

    Upgrade to user API version 1.1

--- lib/acts_as_mailchimp.rb
+++ lib/acts_as_mailchimp.rb
@@ -35,77 +35,77 @@ module Terra
       module InstanceMethods
         # Add a user to a MailChimp mailing list
         def add_to_chimp(list_name, double_opt = false)
-          uid ||= mcLogin(monkeybrains[:username], monkeybrains[:password])
-          list_id ||= get_list_by_name(uid, list_name)
+          apikey ||= mcLogin(monkeybrains[:username], monkeybrains[:password])
+          list_id ||= get_list_by_name(apikey, list_name)
           vars = {}
           vars.merge!({"FNAME" => self[fname_column]}) if self.has_attribute?(fname_column)
           vars.merge!({"LNAME" => self[lname_column]}) if self.has_attribute?(lname_column)
-          mcSubscribe(uid, list_id["id"], self[email_column], vars, self.class.type_column, double_opt)
+          mcSubscribe(apikey, list_id["id"], self[email_column], vars, self.class.type_column, double_opt)
         rescue XMLRPC::FaultException
         end
         
         # Remove a user from a MailChimp mailing list
         def remove_from_chimp(list_name)
-          uid ||= mcLogin(monkeybrains[:username], monkeybrains[:password])
-          list_id ||= get_list_by_name(uid, list_name)
-          mcUnsubscribe(uid, list_id["id"], self[email_column])
+          apikey ||= mcLogin(monkeybrains[:username], monkeybrains[:password])
+          list_id ||= get_list_by_name(apikey, list_name)
+          mcUnsubscribe(apikey, list_id["id"], self[email_column])
         rescue XMLRPC::FaultException
         end
         
         # Update user information at MailChimp
         def update_chimp(list_name, old_email = self[email_column])
-          uid ||= mcLogin(monkeybrains[:username], monkeybrains[:password])
-          list_id ||= get_list_by_name(uid, list_name)
+          apikey ||= mcLogin(monkeybrains[:username], monkeybrains[:password])
+          list_id ||= get_list_by_name(apikey, list_name)
           vars = {}
           vars.merge!({"FNAME" => self[fname_column]}) if self.has_attribute?(fname_column)
           vars.merge!({"LNAME" => self[lname_column]}) if self.has_attribute?(lname_column)
           vars.merge!({"EMAIL" => self[email_column]})
-          mcUpdate(uid, list_id["id"], old_email, vars, self[type_column], true)
+          mcUpdate(apikey, list_id["id"], old_email, vars, self[type_column], true)
         rescue XMLRPC::FaultException
         end
         
         # Log in to MailChimp
         def mcLogin(username, password)
           raise MailChimpConfigError("Please provide a valid user and password") if (username.nil? || password.nil?) 
-          mcAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.0/")
+          mcAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.1/")
           mcAPI.call("login", username, password)
         end
         
         # Subscribe the provided email to a list
-        def mcSubscribe(uid, list_id, email, merge_vars, content_type = 'html', double_opt = true)
-          raise_errors(uid, list_id)
-          mcAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.0/")
-          mcAPI.call("listSubscribe", uid, list_id, email, merge_vars, content_type, double_opt)
+        def mcSubscribe(apikey, list_id, email, merge_vars, content_type = 'html', double_opt = true)
+          raise_errors(apikey, list_id)
+          mcAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.1/")
+          mcAPI.call("listSubscribe", apikey, list_id, email, merge_vars, content_type, double_opt)
         end
         
-        def mcUnsubscribe(uid, list_id, email, delete_user = false, send_goodbye = true, send_notify = true)
-          raise_errors(uid, list_id)
-          mcAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.0/")
-          mcAPI.call("listUnsubscribe", uid, list_id, email, delete_user, send_goodbye, send_notify)
+        def mcUnsubscribe(apikey, list_id, email, delete_user = false, send_goodbye = true, send_notify = true)
+          raise_errors(apikey, list_id)
+          mcAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.1/")
+          mcAPI.call("listUnsubscribe", apikey, list_id, email, delete_user, send_goodbye, send_notify)
         end
         
-        def mcUpdate(uid, list_id, email, merge_vars, content_type = 'html', replace_interests = true)
-          raise_errors(uid, list_id)
-          mcAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.0/")
-          mcAPI.call("listUpdateMember", uid, list_id, email, merge_vars, content_type, replace_interests)
+        def mcUpdate(apikey, list_id, email, merge_vars, content_type = 'html', replace_interests = true)
+          raise_errors(apikey, list_id)
+          mcAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.1/")
+          mcAPI.call("listUpdateMember", apikey, list_id, email, merge_vars, content_type, replace_interests)
         end
         
-        def get_list_by_name(uid, list_name)
+        def get_list_by_name(apikey, list_name)
           raise MailChimpConfigError("Please provide a mailing list name") if list_name.nil?
-          mailing_lists ||= get_all_lists(uid)
+          mailing_lists ||= get_all_lists(apikey)
           unless mailing_lists.nil?  
             mailing_lists.find { |list| list["name"] == list_name }
           end
         end
         
-        def get_all_lists(uid)
-          raise MailChimpConnectError("Please login to MailChimp and make sure you have a valid user ID") if (uid.nil?) 
-          mcAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.0/")
-          mcAPI.call("lists", uid)
+        def get_all_lists(apikey)
+          raise MailChimpConnectError("Please login to MailChimp and make sure you have a valid user ID") if (apikey.nil?) 
+          mcAPI ||= XMLRPC::Client.new2("http://api.mailchimp.com/1.1/")
+          mcAPI.call("lists", apikey)
         end
         
-        def raise_errors(uid, list_id)
-          raise MailChimpConnectError("Please login to MailChimp and make sure you have a valid user ID") if uid.nil?
+        def raise_errors(apikey, list_id)
+          raise MailChimpConnectError("Please login to MailChimp and make sure you have a valid user ID") if apikey.nil?
           raise MailChimpConfigError("Please provide a valid mailing list ID") if list_id.nil?
         end
         
